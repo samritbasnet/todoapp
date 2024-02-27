06d03876b7ad4e61db8b18e7eefa6ec4bdcfc777
samritbasnet <hereiamsamrit7@outlook.com>
2024-02-27 16:45:20 -0500

    adding functionality for add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,40 @@
+import React from "react";
+import { useState } from "react";
 
 import "../src/todo.css";
 import "../src/index.css";
+
+
+
 function App() {
+  const [tasks,setTasks]=useState([]);
+  const [newTask,setNewTask]=useState("");
+
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask(""); 
+    }
+  };
 
   return (
  
       <div>
       <h1 className='title'>Todo List app</h1>
         <label className='label'>Items to Add</label>
-        <input className='input'  type='text' name="item" placeholder='Add new task'/> 
-      <button className='addbtn'>Add an item</button>
+        <input className='input'  type='text' name="item" placeholder='Add new task'
+          value={newTask}
+          onChange={(e)=>setNewTask(e.target.value)}
+        /> 
+      <button className='addbtn' onClick={addTask}>Add an item</button>
     
       <p className='todo'>Todo List</p>
+      <ul>
+        {tasks.map((task,index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
       </div>
   )
 }
